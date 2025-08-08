c0cd0d68c4754534775e303a5d167dc41c6534be
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2025-08-08 16:13:56 -0600

    microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 
 import Clarity from "@microsoft/clarity";
+import Script from "next/script";
 const projectId = process.env.CLARITY_PROJECT_ID;
 
 export const metadata: Metadata = {
@@ -39,7 +40,23 @@ export default function RootLayout({
 
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+      <head>
+        {/* Microsoft Clarity Script */}
+        <Script
+          type="text/javascript"
+          id="ms-clarity-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${projectId}");
+            `,
+          }}
+        />
+      </head>
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
